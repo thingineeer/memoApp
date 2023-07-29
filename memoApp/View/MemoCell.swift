68d522a60f8f2b5900ef6b85f9c0771808d2ee5c
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-07-29 22:47:00 +0900

    feat: updateButtonTapped 구현

--- memoApp/View/MemoCell.swift
+++ memoApp/View/MemoCell.swift
@@ -53,12 +53,19 @@ final class MemoCell: UITableViewCell {
         
     }
     
-    
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
+    
+    
+    @IBAction func updateButtonTapped(_ sender: UIButton) {
+        
+        updateButtonPressed(self)
+    }
+    
+    
 
 }
