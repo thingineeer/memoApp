06a0ef6967b04d5aed3a226f29d9fcf565e1ad8b
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-07-29 21:46:38 +0900

    feat: 필요한 객체 생성 ( appDelegate, context, modelName)

--- memoApp/Model/CoreDataManager.swift
+++ memoApp/Model/CoreDataManager.swift
@@ -10,8 +10,17 @@ import CoreData
 
 final class CoreDataManager {
     
+    
+    // 싱글톤
     static let shared = CoreDataManager()
     private init() {}
     
+    // 앱 델리게이트
+    let appDelegate = UIApplication.shared.delegate as? AppDelegate
     
+    // 임시저장소 context
+    lazy var context = appDelegate?.persistentContainer.viewContext
+    
+    // 엔티티 이름 (코어데이터에 저장된 객체)
+    let modelName: String = "MemoData"
 }
