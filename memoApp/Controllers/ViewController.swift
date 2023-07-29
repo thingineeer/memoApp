38c62ef7bc73fe9d4748454dc35f19dd3c472a9f
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-07-29 21:27:23 +0900

    feat: CoreDataManager클래스 Singleton 으로 추가

---
+++ memoApp/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  memoApp
+//
+//  Created by 이명진 on 2023/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
+
---
+++ memoApp/Model/CoreDataManager.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataManager.swift
+//  memoApp
+//
+//  Created by 이명진 on 2023/07/29.
+//
+
+import UIKit
+import CoreData
+
+final class CoreDataManager {
+    
+    static let shared = CoreDataManager()
+    private init() {}
+    
+    
+}
