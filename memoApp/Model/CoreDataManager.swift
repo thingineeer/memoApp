a93273f95f56e6eb464f044c2f570aa57311460c
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-07-29 23:31:27 +0900

    feat: 메모 조회 기능 추가

--- memoApp/Model/CoreDataManager.swift
+++ memoApp/Model/CoreDataManager.swift
@@ -23,4 +23,28 @@ final class CoreDataManager {
     
     // 엔티티 이름 (코어데이터에 저장된 객체)
     let modelName: String = "MemoData"
+    
+    // MARK: - [Read] 코어데이터에 저장된 데이터 모두 읽어오기
+    func getToDoListFromCoreData() -> [MemoData] {
+        var memoList: [MemoData] = []
+        // 임시저장소 있는지 확인
+        if let context = context {
+            // 요청서
+            let request = NSFetchRequest<NSManagedObject>(entityName: self.modelName)
+            // 정렬순서를 정해서 요청서에 넘겨주기
+            let dateOrder = NSSortDescriptor(key: "date", ascending: false)
+            request.sortDescriptors = [dateOrder]
+            
+            do {
+                // 임시저장소에서 (요청서를 통해서) 데이터 가져오기 (fetch메서드)
+                if let fetchedToDoList = try context.fetch(request) as? [MemoData] {
+                    memoList = fetchedToDoList
+                }
+            } catch {
+                print("가져오는 것 실패")
+            }
+        }
+        
+        return memoList
+    }
 }
